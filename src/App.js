8c605de81da90742793a4f72965a8737d7710e6f
Ori <orikessler@gmail.com>
2022-10-18 20:03:13 +0300

    test the api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
+import axios, { Axios } from 'axios';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const url="http://127.0.0.1:5000/result"
+  const [data,setData]= useState({the_q: ""})
 
+  const handleSumbit=(e)=>{
+    Axios.post(url, {the_q: data.the_q}).then(res=>{console.log(res.data)})
+  }
+
+  const handle=(e)=>{
+    const newData={...data}
+    newData[e.target.id]=e.target.value;
+    setData(newData);
+    console.log(newData);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={"https://lh3.googleusercontent.com/drive-viewer/AJc5JmR6o6nAbJOIAOrS5PuKLQRYBvudbW7sKRcSTNBPlxVqP7EhnoywIz0v1e58-zUOpO24kQojH8o=w1920-h937"} className="App-logo" alt="logo" />
         <form onSubmit={handleSumbit}>
-           <input type="text" name="theQuestion" br/>
+           <input onChange={(e)=> handle(e)} type="text" id="the_q" value={data.the_q} placeholder="the_q"/>
         <button>search</button>
         </form>
       </header>
     </div>
   );
 }
-
-const handleSumbit=(e)=>{
-  const first=e.target.theQuestion.value;
-  console.log(""+first);
-}
-
 export default App;
