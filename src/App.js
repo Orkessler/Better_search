f777ac3b1a4c4a33c19be3000fedfed6ff8b1b6e
Ori <orikessler@gmail.com>
2022-10-23 17:11:04 +0300

    no changes

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={"https://lh3.googleusercontent.com/drive-viewer/AJc5JmR6o6nAbJOIAOrS5PuKLQRYBvudbW7sKRcSTNBPlxVqP7EhnoywIz0v1e58-zUOpO24kQojH8o=w1920-h937"} className="App-logo" alt="logo" />
+        <img src="https://lh3.googleusercontent.com/drive-viewer/AJc5JmR6o6nAbJOIAOrS5PuKLQRYBvudbW7sKRcSTNBPlxVqP7EhnoywIz0v1e58-zUOpO24kQojH8o=w1920-h937" className="App-logo" alt="logo" />
         <form onSubmit={handleSumbit}>
            <input onChange={(e)=> handle(e)} type="text" id="the_q" value={data.the_q} />
         <button>search</button>
