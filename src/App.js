c3c8cb69b8c25d3d8997817b654e2a7fcadee7c9
Ori Kessler <orikessler@gmail.com>
2022-11-07 18:21:55 +0200

    logo, rtl input and css fix

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@
 import  Axios  from 'axios';
 import './App.css';
 import React, {useState} from 'react';
+import Iframe from 'react-iframe';
+import myLogo from './better_logo.png';
 
 function App() {
   const url="http://127.0.0.1:5000/result"
-  const [data,setData]= useState({the_q:""})
+  const [data,setData]= useState({the_q:null})
+  const [showResults, setShowResults] = React.useState(false)
 
   const handleSumbit=(e)=>{
     e.preventDefault();
-    Axios.post(url,{the_q: data.the_q})//.then(res=>{console.log(res.data)})
+    Axios.post(url,{the_q: data.the_q});//.then(res=>{console.log(res.data)})
+    data.the_q&&setShowResults(true);
   }
 
   const handle=(e)=>{
@@ -19,16 +23,33 @@ function App() {
     console.log(newData);
     //Axios.post(url, {the_q: data.the_q})//.then(res=>{console.log(res.data)})    
   }
+  const Results = () => (
+    <div id="results" className="search-results">
+      <script async src="https://cse.google.com/cse?cx=23c24c7e71d72499d">
+     </script>
+    <div class="gcse-search"></div>
+        <Iframe url="https://www.n12.co.il/"  width="90%" height="300" ></Iframe>
+    </div>
+  )
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://raw.githubusercontent.com/Orkessler/my_search/master/better_logo.png?token=GHSAT0AAAAAAB2CR3CPPE2SLIWJX2EHJ3AGY2ZH5FA" className="App-logo" alt="logo" />
+        <img src={myLogo} className="App-logo" alt="logo" />
         <form onSubmit={handleSumbit}>
-           <input onChange={(e)=> handle(e)} type="text" id="the_q" value={data.the_q} />
-        <button>search</button>
+          <div>
+             <input onChange={(e)=> handle(e)} dir="rtl" type="text" id="the_q" value={data.the_q} className="App-input" />
+          </div>
+        <button>חפש</button>
         </form>
       </header>
+      <body> 
+      <script async src="https://cse.google.com/cse.js?cx=23c24c7e71d72499d">
+     </script>
+     <div class="gcse-search"></div>
+      </body>
+      { showResults ? <Results/> : null }
     </div>
   );
 }
