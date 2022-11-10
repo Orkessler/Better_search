7202d0f5917be8c2db9770d0479394f75f108068
Ori Kessler <orikessler@gmail.com>
2022-11-10 17:57:56 +0200

    after fix cors problem

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@
 import  Axios  from 'axios';
 import './App.css';
 import React, {useState} from 'react';
-import Iframe from 'react-iframe';
+//import Iframe from 'react-iframe';
 import myLogo from './better_logo.png';
 
 function App() {
-  const url="http://127.0.0.1:5000/result"
-  const [data,setData]= useState({the_q:''})
+  const url="https://googlepp.orikessler.repl.co/result"
+  const [data,setData]= useState({the_q:'',the_ans:''})
   const [showResults, setShowResults] = React.useState(false)
 
   const handleSumbit=(e)=>{
     e.preventDefault();
-    Axios.post(url,{the_q: data.the_q});//.then(res=>{console.log(res.data)})
+    Axios.post(url,{the_q: data.the_q}).then(res=>{console.log(res.data)})
+    this.data.the_ans=Axios.post(url,{the_q: data.the_q})
     data.the_q!==''&&setShowResults(true);
   }
 
@@ -21,12 +22,11 @@ function App() {
     newData[e.target.id]=e.target.value;
     setData(newData);
     console.log(newData);
-    //Axios.post(url, {the_q: data.the_q})//.then(res=>{console.log(res.data)})    
   }
   const Results = () => (
     <div>
          <h1>{data.the_q}</h1>
-        <Iframe url="https://www.n12.co.il/"  width="90%" height="300" ></Iframe>
+         {/*< Iframe url="https://www.e-vrit.co.il/Product/26136/%D7%97%D7%99%D7%99%D7%9D"  width="90%" height="300" ></Iframe>*/}
         </div>
   )
 
@@ -37,12 +37,13 @@ function App() {
         <img src={myLogo} className="App-logo" alt="logo" />
         <form onSubmit={handleSumbit}>
           <div>
-             <input onChange={(e)=> handle(e)} dir="rtl" type="text" id="the_q" value={data.the_q} className="App-input" />
+             <input onChange={(e)=> handle(e)} dir="rtl" type="text" id="the_q" className="App-input" />
           </div>
         <button>חפש</button>
         </form>
+        { showResults ? <Results/> : null }
       </header>
-      { showResults ? <Results/> : null }
+      
     </div>
   );
 }
